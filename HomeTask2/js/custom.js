44554c37276935cfb31412da53f37776e17db51e
Vidushi Aggarwal <vidushi_aggarwal@epam.com>
2022-06-24 22:13:57 +0530

    hometask

---
+++ HomeTask2/js/custom.js
@@ -0,0 +1,55 @@
+(function() {
+  var burger = document.querySelector(".burger");
+  var menu = document.querySelector("#" + burger.dataset.target);
+  burger.addEventListener("click", function() {
+    burger.classList.toggle("is-active");
+    menu.classList.toggle("is-active");
+  });
+})();
+
+document.querySelectorAll("#nav li").forEach(function(navEl) {
+  navEl.onclick = function() {
+    toggleTab(this.id, this.dataset.target);
+  };
+});
+
+function toggleTab(selectedNav, targetId) {
+  var navEls = document.querySelectorAll("#nav li");
+
+  navEls.forEach(function(navEl) {
+    if (navEl.id == selectedNav) {
+      navEl.classList.add("is-active");
+    } else {
+      if (navEl.classList.contains("is-active")) {
+        navEl.classList.remove("is-active");
+      }
+    }
+  });
+
+  var tabs = document.querySelectorAll(".tab-pane");
+
+  tabs.forEach(function(tab) {
+    if (tab.id == targetId) {
+      tab.style.display = "block";
+    } else {
+      tab.style.display = "none";
+    }
+  });
+}
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
